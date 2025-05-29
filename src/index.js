2a56958db71727263a33c6270902810f477bc987
Your Name <doantrongvinhse@gmail.com>
2025-05-29 16:59:33 +0700

    hhe

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const routes = require("./routes");
 const connectDB = require("./config/db");
+const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
 const port = process.env.Port || 3001;
 
 // Mount the routes middleware
+app.use(cors());    
 app.use(express.json());
 app.use('/', routes);
-// app.use(cors());    
 
 connectDB();
 
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/ProductController');
 
 router.get('/products', productController.getAllProducts);
+router.get('/products/:id', productController.getProductById);
 router.post('/products', productController.createProduct);
 router.put('/products/:id', productController.updateProduct);
 router.delete('/products/:id', productController.deleteProduct);
