290035de23f4b5db370bc5aeff453830bd4a659f
VDoan <doantrongvinhse@gmail.com>
2025-06-02 18:09:22 +0700

    update search

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/ProductController');
 
+router.get('/products/search', productController.searchProducts);
 router.get('/products', productController.getAllProducts);
 router.get('/products/:id', productController.getProductById);
 router.post('/products', productController.createProduct);
 router.put('/products/:id', productController.updateProduct);
 router.delete('/products/:id', productController.deleteProduct);
 router.get('/products/category/:categoryId', productController.getProductsByCategory);
-router.get('/products/search', productController.searchProducts);
 module.exports = router;
 
 
